43f136c93df35fea5be6ca831766767cf0050d92
khaldi505 <habetlbrika69@gmail.com>
2021-07-11 13:31:06 -0700

    3-homepage.html

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $.ajax(
       $(".f-loader").remove();
     });
 
-
+// popular tuto script
     $.ajax( 
       {type: "GET", url: "https://smileschool-api.hbtn.info/popular-tutorials",
       dataType: 'json',
@@ -83,4 +83,56 @@ $.ajax(
           }
              
           });
+          // latest section script
+          $.ajax( 
+            {type: "GET", url: "https://smileschool-api.hbtn.info/latest-videos",
+            dataType: 'json',
+            data: {
+              action: "query",
+              list: "search",
+              format: "json"
+              }}).done(function(result){
+                function star(stars, author){
+      
+                  span = document.createElement("span")
+                      $(span).addClass("col-md-12")
+                      for (let starDone = 0; starDone < stars; starDone++){
+                        $(span).append(`<img src="images/star_on.png" alt="star_off" clas="col-1">`)
+                      }
+      
+                      for (let totalStars = 5; totalStars > stars ; totalStars--) { 
+                      $(span).append(`<img src="images/star_off.png" alt="star_off" clas="col-1">`)
+                    }
+                      return span;
+                }
+                
+              for(let i = 0, l = result.length; i < l; i++) {
+                card_author = result[i].title.split(" ")
+                card_author = card_author[0]
+                card = $(`
+                <div class="card card-style col-md-3 col-sm-6 col-xs-12 border-0" style="width: 18rem;">
+                            <span><img class="card-img-top" src=${result[i].thumb_url} alt="Card image cap"><img src="images/play.png" class="play-icon" alt="play"></span>
+                          <div class="card-body ${card_author}">
+                            <h5 class="card-title"><b>${result[i].title}</b></h5>
+                            <p class="card-text text-secondary">${result[i]['sub-title']}</p>
+                            <div class="row">
+                              <img src=${result[i].author_pic_url} alt="" class="rounded-circle col-3" width=35px height="35px">
+                              <h6 class="purple pt-2">${result[i].author}</h6>
+                            </div>
+                          </div>
+                        </div>`)
+                        
+                stars = $(star(result[i].star, card_author));
+                card.append(stars);
+                card.append(`<p class="ml-auto" style="color: #C271FF;">${result[i].duration}</p>`)
+                    if (result[i].id <= 4){$(card).appendTo(".fs-row")}
+                    else if (result[i].id >= 5 && result[i].id <= 8){$(card).appendTo(".ss-row")}
+                    
+                    $(".t-loader").remove();
+       
+                
+                     
+                }
+                   
+                });
         })
